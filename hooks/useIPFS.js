fe464ae96c99ef068db6c271b78f2cd30d8e8f2b
maweiche <maweiche@gmail.com>
2022-05-31 17:05:09 -0400

    updated package for web3.storage and removed ipfs-http. updated hook

--- hooks/useIPFS.js
+++ hooks/useIPFS.js
@@ -1,14 +1,5 @@
-import { useState, useEffect } from 'react';
-import { Web3Storage } from 'web3.storage';
-
 const useIPFS = (hash, filename) => {
-  const [file, setFile] = useState(null);
-
-  useEffect(() => {
-    setFile(`https://ipfs.io/ipfs/${hash}/${filename}`);
-  }, []);
-
-  return file;
+    return `https://ipfs.io/ipfs/${hash}/${filename}`
 };
 
 export default useIPFS;
