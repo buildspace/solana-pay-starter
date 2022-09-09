9ce50df1a22bd0de70d17890354e0ec4effba2de
candycode-cc <JhIB87BubjU!nkh(-!>
2022-09-09 03:43:33 +0200

    test

--- pages/index.js
+++ pages/index.js
@@ -1,38 +1,82 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import CreateProduct from "../components/CreateProduct";
+import Product from "../components/Product";
 import HeadComponent from '../components/Head';
+
+import { useWallet } from "@solana/wallet-adapter-react";
+import { WalletMultiButton } from "@solana/wallet-adapter-react-ui";
 
 // Constants
 const TWITTER_HANDLE = "_buildspace";
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 
 const App = () => {
-  
-  
-  return (
-    <div className="App">
-      <HeadComponent/>
-      <div className="container">
-        <header className="header-container">
-          <p className="header"> 😳 Buildspace Emoji Store 😈</p>
-          <p className="sub-text">The only emoji store that accepts sh*tcoins</p>
-        </header>
+    const { publicKey } = useWallet();
+    const isOwner = (publicKey ? publicKey.toString() === process.env.NEXT_PUBLIC_OWNER_PUBLIC_KEY : false);
+    const [creating, setCreating] = useState(false);
+    const [products, setProducts] = useState([]);
 
-        <main>
-          <img src="https://media.giphy.com/media/eSwGh3YK54JKU/giphy.gif" alt="emoji" />
-        </main>
+    const renderNotConnectedContainer = () => (
+        <div>
+            <img src="https://media.giphy.com/media/eSwGh3YK54JKU/giphy.gif" alt="emoji" />
 
-        <div className="footer-container">
-          <img alt="Twitter Logo" className="twitter-logo" src="twitter-logo.svg" />
-          <a
-            className="footer-text"
-            href={TWITTER_LINK}
-            target="_blank"
-            rel="noreferrer"
-          >{`built on @${TWITTER_HANDLE}`}</a>
+            <div className="button-container">
+                <WalletMultiButton className="cta-button connect-wallet-button" />
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
+
+    useEffect(() => {
+        if (publicKey) {
+            fetch(`/api/fetchProducts`)
+                .then(response => response.json())
+                .then(data => {
+                    setProducts(data);
+                    console.log("Products", data);
+                });
+        }
+    }, [publicKey]);
+
+    const renderItemBuyContainer = () => (
+        <div className="products-container">
+            {products.map((product) => (
+                <Product key={product.id} product={product} />
+            ))}
+        </div>
+    );
+
+    return (
+        <div className="App">
+            <HeadComponent />
+            <div className="container">
+                <header className="header-container">
+                    <p className="header"> 😳 Buildspace Emoji Store 😈</p>
+                    <p className="sub-text">The only emoji store that accepts shitcoins</p>
+
+                    {isOwner && (
+                        <button className="create-product-button" onClick={() => setCreating(!creating)}>
+                            {creating ? "Close" : "Create Product"}
+                        </button>
+                    )}
+                </header>
+
+                <main>
+                    {creating && <CreateProduct />}
+                    {publicKey ? renderItemBuyContainer() : renderNotConnectedContainer()}
+                </main>
+
+                <div className="footer-container">
+                    <img alt="Twitter Logo" className="twitter-logo" src="twitter-logo.svg" />
+                    <a
+                        className="footer-text"
+                        href={TWITTER_LINK}
+                        target="_blank"
+                        rel="noreferrer"
+                    >{`built on @${TWITTER_HANDLE}`}</a>
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default App;
