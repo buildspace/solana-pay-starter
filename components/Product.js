6228fb40eb27e9ef1ad4955b6577a1026c9b3d9a
Allen Yao <allentyao@gmail.com>
2023-01-09 00:50:07 +0000

    connected to IPFS

---
+++ components/Product.js
@@ -0,0 +1,28 @@
+import React from "react";
+import styles from "../styles/Product.module.css";
+import IPFSDownload from './IpfsDownload';
+
+export default function Product({ product }) {
+  const { id, name, price, description, image_url } = product;
+
+  return (
+    <div className={styles.product_container}>
+      <div >
+        <img className={styles.product_image}src={image_url} alt={name} />
+      </div>
+
+      <div className={styles.product_details}>
+        <div className={styles.product_text}>
+          <div className={styles.product_title}>{name}</div>
+          <div className={styles.product_description}>{description}</div>
+        </div>
+
+        <div className={styles.product_action}>
+          <div className={styles.product_price}>{price} USDC</div>
+          {/* I'm hardcoding these for now, we'll fetch the hash from the API later*/}
+          <IPFSDownload filename="ghosts.zip" hash="QmZ1JBpC2nwGd4jmNBXL33sFVCLQfKEDTSKgQAGVvQaRbQ" cta="Catch Some Ghosts!"/>
+        </div>
+      </div>
+    </div>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,52 @@
-import React from "react";
-import HeadComponent from '../components/Head';
+import React, { useEffect, useState } from 'react';
+import Product from "../components/Product";
+
+import { useWallet } from '@solana/wallet-adapter-react';
+import { WalletMultiButton } from '@solana/wallet-adapter-react-ui';
 
 // Constants
-const TWITTER_HANDLE = "_buildspace";
+const TWITTER_HANDLE = '_buildspace';
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 
 const App = () => {
+  const { publicKey } = useWallet();
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    if (publicKey) {
+      fetch(`/api/fetchProducts`)
+        .then(response => response.json())
+        .then(data => {
+          setProducts(data);
+          console.log("Products", data);
+        });
+    }
+  }, [publicKey]);
+
+  const renderNotConnectedContainer = () => (
+    <div className="button-container">
+      <WalletMultiButton className="cta-button connect-wallet-button" />
+    </div>
+  );
   
-  
+  const renderItemBuyContainer = () => (
+    <div className="products-container">
+      {products.map((product) => (
+        <Product key={product.id} product={product} />
+      ))}
+    </div>
+  );
+
   return (
     <div className="App">
-      <HeadComponent/>
       <div className="container">
         <header className="header-container">
           <p className="header"> 😳 Buildspace Emoji Store 😈</p>
-          <p className="sub-text">The only emoji store that accepts sh*tcoins</p>
+          <p className="sub-text">The only emoji store that accepts shitcoins</p>
         </header>
 
         <main>
-          <img src="https://media.giphy.com/media/eSwGh3YK54JKU/giphy.gif" alt="emoji" />
+          {publicKey ? renderItemBuyContainer() : renderNotConnectedContainer()}
         </main>
 
         <div className="footer-container">
