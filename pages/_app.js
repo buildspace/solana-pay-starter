553edbdbf1810ca4806a488a1d3534fb80580374
Sean <seanlengws@gmail.com>
2023-01-21 23:38:59 +0800

    fix: updated package version to the latest

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,9 @@
-import React, { useMemo } from "react";
+import React from "react";
 import "../styles/globals.css";
 import "../styles/App.css";
 
 const App = ({ Component, pageProps }) => {
-
-  return (
-    <Component {...pageProps} />
-  );
+  return <Component {...pageProps} />;
 };
 
 export default App;
